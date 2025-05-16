de65cb2e756ae9116c6dd8323530cb8dcee50aec
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2025-05-16 18:03:19 +0200

    Correct App names to match Django conventions.

--- OpenAIChat/models.py
+++ OpenAIChat/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from UserProfile.models import UserProfile
+from userprofile.models import UserProfile
 
 class Message(models.Model):
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
--- tech_buddy_project/urls.py
+++ tech_buddy_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('aichat/', include('OpenAIChat.urls')),
+    path('aichat/', include('openaichat.urls')),
 ]
